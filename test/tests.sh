402c369177d1a60d6bf5a24e771030be492b0618
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 05:00:22 +0300

    add test for TBI functionality

--- test/tests.sh
+++ test/tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
 cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
+cat stdin/history_sample.log |node ../index.js --last 2 > stdout/last-2.out
 echo "Nice. Now run 'git diff' for deviations"
