75954168d34c329d99fbeacc56d820b13045c726
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 05:15:36 +0300

    Allow tests.sh to be called outside its root. Document

--- test/tests.sh
+++ test/tests.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# Make sure we are in the test root in case the script was called from outside the root
+cd ${BASH_SOURCE%/*}
+
+# Run a few test cases
 cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
 cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
 cat stdin/history_sample.log |node ../index.js --last 2 > stdout/last-2.out
-echo "Nice. Now run 'git diff' for deviations"
+
+# Show test result deviations
+git diff
+
+# Tidy up
+cd -
