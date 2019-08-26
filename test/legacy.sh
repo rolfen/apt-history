009f8e9f5f7e51f20e5301207937fc00d1ead9b7
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 05:50:37 +0300

    add test

--- test/legacy.sh
+++ test/legacy.sh
@@ -7,6 +7,7 @@ cd ${BASH_SOURCE%/*}
 cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
 cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
 cat stdin/history_sample.log |node ../index.js 29 > stdout/single-record.out
+cat stdin/history_sample.log |node ../index.js 29 Install --as-apt-arguments > stdout/as-apt-arguments.out
 
 # Show test result deviations
 git diff ./stdout
