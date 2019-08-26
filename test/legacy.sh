b60ef880bfaebc58719a3068b863b28944cdedeb
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 05:38:07 +0300

    split tests into legacy or developed functionality

---
+++ test/legacy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make sure we are in the test root in case the script was called from outside the root
+cd ${BASH_SOURCE%/*}
+
+# Run a few test cases
+cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
+cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
+cat stdin/history_sample.log |node ../index.js 29 > stdout/single-record.out
+
+# Show test result deviations
+git diff ./stdout
+
+# Tidy up
+cd -
---
+++ test/new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make sure we are in the test root in case the script was called from outside the root
+cd ${BASH_SOURCE%/*}
+
+# Run a few test cases
+cat stdin/history_sample.log |node ../index.js --last 2 > stdout/last-2.out
+
+# Show test result deviations
+git diff ./stdout
+
+# Tidy up
+cd -
