84b7523d55e78470f1f6c80ef025d092247b62a2
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 04:49:58 +0300

    reword

--- test/tests.sh
+++ test/tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
 cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
-echo "Nice. Now run 'git diff' to see test results"
+echo "Nice. Now run 'git diff' for deviations"
