19bc1420f97544b2c4448988f2325996625c94ae
rolfen <rolf.dergham.public@gmail.com>
2019-08-26 05:31:02 +0300

    Add test for single record view

--- test/tests.sh
+++ test/tests.sh
@@ -7,6 +7,7 @@ cd ${BASH_SOURCE%/*}
 cat stdin/history_sample.log |node ../index.js > stdout/no-args.out
 cat stdin/history_sample.log |node ../index.js from 1 > stdout/first.out
 cat stdin/history_sample.log |node ../index.js --last 2 > stdout/last-2.out
+cat stdin/history_sample.log |node ../index.js 29 > stdout/single-record.out
 
 # Show test result deviations
 git diff
