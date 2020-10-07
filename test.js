b99a9e48f3ce0cd1a73d854a513027ed134d60bb
Rolf <NA>
2020-10-07 17:46:32 +0300

    add vanilla sample unit test

---
+++ test.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const argv = require('minimist')(process.argv.slice(2));
+const assert = require('assert').strict;
+const lib = require('./lib.js');
+
+testGetEnv();
+
+function testGetEnv() {
+	const getEnv = lib.getEnv;
+	var env;
+
+	env = lib.getEnv({
+		'_' : [23]
+	})
+	assert.equal(env.isListMode, false, "Numerical first argument triggers item mode")
+
+	env = lib.getEnv({
+		'_' : ['SomeProp']
+	})
+	assert.equal(env.selectedProperty, 'SomeProp', "Manually select property")
+
+
+}
